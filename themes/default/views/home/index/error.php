a839c6e223a52b1d83f9bc04751af37ad0a7a37f
xiaomalover <xiaomalover@gmail.com>
2019-01-29 14:50:28 +0800

    update

--- themes/default/views/home/index/error.php
+++ themes/default/views/home/index/error.php
@@ -10,19 +10,15 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<section id="portfolio">
+    <div class="container">
+        <div class="row">
+            <div class="heading text-center col-sm-8 col-sm-offset-2 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+                <h1>啊哟，貌似出现了错误</h1>
+                <h2 style="color:#ff9933;"><?php print_r($exception->statusCode); ?></h2>
+                <p><?= nl2br(Html::encode($message)) ?></p>
+            </div>
+        </div>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
+</section>
