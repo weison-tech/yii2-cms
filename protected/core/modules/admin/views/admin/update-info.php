a4867abb104ba12a6700ee2600e2c190adddbe8b
xiaomalover <xiaomalover@gmail.com>
2019-05-29 16:39:20 +0800

    update

--- protected/core/modules/admin/views/admin/update-info.php
+++ protected/core/modules/admin/views/admin/update-info.php
@@ -11,26 +11,24 @@ $this->title = Yii::t('AdminModule.base', 'Update Info');
 $this->params['breadcrumbs'][] = Yii::t('AdminModule.base', 'Update Info');
 ?>
 
-<div class="admin-form box box-primary">
-    <?php $form = ActiveForm::begin(); ?>
-    <div class="box-body table-responsive">
+<?php $form = ActiveForm::begin(); ?>
+<div class="box-body table-responsive">
 
-        <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
 
-        <?= $form->field($model, 'repassword')->passwordInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'repassword')->passwordInput(['maxlength' => true]) ?>
 
-        <?= $form->field($model, 'avatar')->widget('core\modules\file\widgets\Upload',
-            [
-                'url' => ['/file/file/upload'],
-                'maxFileSize' => 10 * 1024 * 1024, // 10 MiB,
-                'minFileSize' => 1,
-                'acceptFileTypes' => new JsExpression('/(\.|\/)(gif|jpe?g|png)$/i'),
-            ]
-        ); ?>
+    <?= $form->field($model, 'avatar')->widget('core\modules\file\widgets\Upload',
+        [
+            'url' => ['/file/file/upload'],
+            'maxFileSize' => 10 * 1024 * 1024, // 10 MiB,
+            'minFileSize' => 1,
+            'acceptFileTypes' => new JsExpression('/(\.|\/)(gif|jpe?g|png)$/i'),
+        ]
+    ); ?>
 
-    </div>
-    <div class="box-footer">
-        <?= Html::submitButton(Yii::t('base', 'Save'), ['class' => 'btn btn-success btn-flat']) ?>
-    </div>
-    <?php ActiveForm::end(); ?>
 </div>
+<div class="box-footer">
+    <?= Html::submitButton(Yii::t('base', 'Save'), ['class' => 'btn btn-success btn-flat']) ?>
+</div>
+<?php ActiveForm::end(); ?>
